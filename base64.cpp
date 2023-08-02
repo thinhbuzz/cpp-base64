ed5db2aaf4e3b71120321e9aaa3081ffb9aa9275
Mr.Buzz <mr.thinhbuzz@gmail.com>
2023-08-02 21:14:51 +0700

    remove throw

--- base64.cpp
+++ base64.cpp
@@ -67,7 +67,7 @@ static unsigned int pos_of_char(const unsigned char chr) {
  // 2020-10-23: Throw std::exception rather than const char*
  //(Pablo Martin-Gomez, https://github.com/Bouska)
  //
-    throw std::runtime_error("Input is not valid base64-encoded data.");
+    std::runtime_error("Input is not valid base64-encoded data.");
 }
 
 static std::string insert_linebreaks(std::string str, size_t distance) {
